74dccfc139bfbf3e433d4e59d188d373a0d9d926
isherrill <me@isaiahsherrill.com>
2022-05-26 21:18:17 -0500

    updating anchor tag to use router-link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="w-screen h-screen bg-gray-500">
     <header class="bg-gradient-to-r from-green-400 to-blue-500 flex flex-row justify-between p-8 text-white font-bold">
       <div class="container">
-        <a class="float-left cursor-pointer hover:text-red-400">WOD TRACKER</a>
+        <router-link class="float-left cursor-pointer hover:text-red-400" to="/">WOD TRACKER</router-link>
         <ul class="float-right flex flex-row space-x-6">
           <li>
             <router-link class="hover:text-red-400" to="/">CREATE WOD</router-link>
